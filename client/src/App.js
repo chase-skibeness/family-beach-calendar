7d2d3d5f862ebe5ac14650b4ad5594bf38d65364
Chase Skibeness <chase.skibeness@limeade.com>
2022-06-03 16:44:14 -0700

    setup containers and calendar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 function App() {
+  const handleDateClick = (arg) => {
+    console.log(arg.dateStr);
+  };
+
   return (
     <div className="App">
-      <header>
-        <h1>Hello World</h1>
-      </header>
+      <div className="Container FormContainer">
+        <header>
+          <h1>Hello World</h1>
+        </header>
+      </div>
+      <div className="Container CalendarContainer">
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          dateClick={handleDateClick}
+          events={[{ title: 'event 1', date: '2022-06-01' }]}
+        />
+      </div>
     </div>
   );
 }
